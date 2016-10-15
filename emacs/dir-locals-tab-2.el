53e0f259125d0e39f7cdcf2069f8c0affd3f22a6
Etienne <notetiene@users.noreply.github.com>
2016-10-14 20:00:47 -0400

    feat: Add default emacs `.dir-locals.el` file
    * emacs/dir-locals-tab-2.el: Configuration for project with indent
      offset of 2.
    * emacs/dir-locals-tab-4.el: Configuration for project with indent
      offset of 4.

---
+++ emacs/dir-locals-tab-2.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (tab-width . 2)
+  (js2-basic-offset . 2)
+  (js-indent-level . 2)
+  (web-mode-markup-indent-offset . 2)
+  (fill-column . 80)
+  (flycheck-eslint-enable . t)))
