5d472453740f24f50951587cff31c9ed4c934222
Etienne <notetiene@users.noreply.github.com>
2017-04-19 19:25:10 -0400

    Remove the `js2-basic-offset` in emacs

--- emacs/dir-locals-tab-2.el
+++ emacs/dir-locals-tab-2.el
@@ -3,7 +3,6 @@
 
 ((nil
   (tab-width . 2)
-  (js2-basic-offset . 2)
   (js-indent-level . 2)
   (web-mode-markup-indent-offset . 2)
   (fill-column . 80)
--- emacs/dir-locals-tab-4.el
+++ emacs/dir-locals-tab-4.el
@@ -3,8 +3,9 @@
 
 ((nil
   (tab-width . 4)
-  (js2-basic-offset . 4)
   (js-indent-level . 4)
   (web-mode-markup-indent-offset . 4)
   (fill-column . 80)
   (flycheck-eslint-enable . t)))
+  (projectile-project-compilation-cmd "npm build")
+  (projectile-project-test-cmd "npm test")))
